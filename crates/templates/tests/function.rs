75b94968a39fb9274ef48084f372f6a4d7bb6ae6
CloudBridge UY <admin@cloudbridge.com.uy>
2023-08-27 16:41:58 -0300

    feat: add default and required option validation

--- crates/templates/tests/function.rs
+++ crates/templates/tests/function.rs
@@ -53,4 +53,162 @@ fn test_function_template() {
             "description": "Simple function",
         }})
     );
+    test_template!(
+        "function.tera",
+        "Function with global required option",
+        serde_json::json!({
+        "options": {
+            "require": {
+                "name": "require",
+                "required": true
+            }
+        },
+        "command": {
+            "name": "foo",
+            "description": "Function with global required option",
+        }})
+    );
+    test_template!(
+        "function.tera",
+        "Function with local required option",
+        serde_json::json!({
+        "command": {
+            "name": "foo",
+            "description": "Function with a local required option",
+                "options": {
+                    "require": {
+                        "name": "require",
+                        "required": true
+                    }
+                },
+            }
+        })
+    );
+    test_template!(
+        "function.tera",
+        "Function with a local and global required options with the same name",
+        serde_json::json!({
+        "options": {
+            "require": {
+                "name": "require",
+                "required": true
+            }
+        },
+        "command": {
+            "name": "foo",
+            "description": "Function with a local required option",
+                "options": {
+                    "require": {
+                        "name": "require",
+                        "required": true
+                    }
+                },
+            }
+        })
+    );
+    test_template!(
+        "function.tera",
+        "Function with a local and two global required options, one with the same name",
+        serde_json::json!({
+        "options": {
+            "foo": {
+                "name": "foo",
+                "required": true
+            },
+            "require": {
+                "name": "require",
+                "required": true
+            }
+        },
+        "command": {
+            "name": "foo",
+            "description": "Function with a local required option",
+                "options": {
+                    "require": {
+                        "name": "require",
+                        "required": true
+                    }
+                },
+            }
+        })
+    );
+    test_template!(
+        "function.tera",
+        "Function with global default option",
+        serde_json::json!({
+        "options": {
+            "default": {
+                "name": "default",
+                "default": "foo"
+            }
+        },
+        "command": {
+            "name": "foo",
+            "description": "Function with global default option",
+        }})
+    );
+    test_template!(
+        "function.tera",
+        "Function with local default option",
+        serde_json::json!({
+        "command": {
+            "name": "foo",
+            "description": "Function with a local default option",
+                "options": {
+                    "default": {
+                        "name": "default",
+                        "default": "foo"
+                    }
+                },
+            }
+        })
+    );
+    test_template!(
+        "function.tera",
+        "Function with a local and global default options with the same name",
+        serde_json::json!({
+        "options": {
+            "default": {
+                "name": "default",
+                "default": "foo"
+            }
+        },
+        "command": {
+            "name": "foo",
+            "description": "Function with a local required option",
+                "options": {
+                    "default": {
+                        "name": "default",
+                        "default": "foo"
+                    }
+                },
+            }
+        })
+    );
+    test_template!(
+        "function.tera",
+        "Function with a local and two global default options, one with the same name",
+        serde_json::json!({
+        "options": {
+            "foo": {
+                "name": "foo",
+                "default": "foo"
+            },
+            "default": {
+                "name": "default",
+                "default": true
+            }
+        },
+        "command": {
+            "name": "foo",
+            "description": "Function with a local default option",
+                "options": {
+                    "default": {
+                        "name": "default",
+                        "default": true
+                    }
+                },
+            }
+        })
+    );
 }
